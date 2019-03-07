43d4b8aac36b1c1b97dcf72068170bae1aa19945
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-07 16:58:31 +0100

    update scraper.

---
+++ parsers/products_search.rb
@@ -0,0 +1,49 @@
+data = JSON.parse(content)
+scrape_url_nbr_products = data['tab_info'][0]['product_info']['p_count'].to_i
+current_page = page['vars']['page']
+products = data['tab_info'][0]['product_info']['products']
+
+
+
+# if ot's first page , generate pagination
+if current_page == 1 and scrape_url_nbr_products > products.length
+  nbr_products_pg1 = products.length
+  step_page = 1
+  while step_page * products.length <= scrape_url_nbr_products
+    step_page = 2
+
+
+    step_page = step_page + 1
+
+
+  end
+elsif current_page == 1 and products.length <= scrape_url_nbr_products
+  nbr_products_pg1 = products.length
+else
+  nbr_products_pg1 = page['vars']['nbr_products_pg1']
+end
+
+
+products.each_with_index do |product, i|
+
+ query = '{"query":"\n      query ProductQuery(\n        $id: Int!\n      ) {\n        product(\n          id: $id\n        ) {\n          base_img_url                  \n          ...productFields\n          children {\n            ...productFields\n          }\n        }\n      }\n      fragment productFields on Product {\n        id\n        desc\n        pack_desc\n        sp\n        mrp\n        w\n        images {\n          s\n          m\n          l\n          xl\n          xxl\n        }\n        variable_weight {\n          msg\n          link\n        }\n        discount {\n          type\n          value\n        }\n        brand {\n          name\n          slug\n          url\n        }\n        additional_attr {\n          food_type\n          info {\n            type\n            image\n            sub_type\n            label\n          }\n        }\n        tabs {\n          content\n          title\n        }\n        tags {\n          header\n          values {\n            display_name\n            dest_type\n            dest_slug\n            url\n          }\n        }\n        combo_info {\n          destination {\n            display_name\n            dest_type\n            dest_slug\n            url\n          }\n          total_saving_msg          \n          items{\n            id\n            brand\n            sp\n            mrp\n            is_express\n            saving_msg\n            link\n            img_url\n            qty\n            wgt\n            p_desc\n          }\n          total_sp\n          total_mrp\n          annotation_msg\n        }\n        gift {\n          msg\n        }\n        sale {\n          type\n          display_message\n          end_time\n          maximum_redem_per_order\n          maximum_redem_per_member\n          show_counter\n          message\n          offers_msg\n        }\n        promo {\n          type\n          label\n          id\n          name\n          saving\n          savings_display\n          desc\n          url\n          desc_label\n        }\n        store_availability {\n          tab_type\n          pstat\n          availability_info_id\n          store_id\n        }\n        discounted_price {\n          display_name\n          value\n        }\n      }\n    ","variables":{"id":"'+product['sku'].to_s+'","visitorId":"824190617","masterRi":"3630","cityId":"1"}}'
+
+  pages << {
+      page_type: 'product_details',
+      method: 'POST',
+      url: "https://www.bigbasket.com/product/pd/v1/gql?search_term=#{page['vars']['search_term']}&page=#{current_page + 1}&rank=#{ i + 1}",
+      body:query,
+      vars: {
+          'input_type' => page['vars']['input_type'],
+          'search_term' => page['vars']['search_term'],
+          'product_rank' => i + 1,
+          'page' => current_page,
+          'nbr_products_pg1' => nbr_products_pg1,
+          'scrape_url_nbr_products' => scrape_url_nbr_products
+
+      }
+  }
+
+
+end
+
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,7 +2,7 @@ require 'cgi'
 pages << {
     page_type: 'products_search',
     method: 'GET',
-    url: 'https://www.bigbasket.com/product/get-products/?sid=MtSGZo6ibmbDoWMBo21hdqUzLjIuMKJjY60zNTF8MTY5NHwxNzA5qHNrdV9saXN0kKJhb8KidXLComFww6JsdM0BG6Nkc2pNoW-qcG9wdWxhcml0ealzb3VyY2VfaWQBomRzzQEvo21yac0OLg==&listtype=pc&filters=[]&sorted_on=pricelth&slug=sports-energy-drinks&tab_type=[%22all%22]',
+    url: 'https://www.bigbasket.com/product/get-products/?sid=MtSGZo6ibmbDoWMBo21hdqUzLjIuMKJjY60zNTF8MTY5NHwxNzA5qHNrdV9saXN0kKJhb8KidXLComFww6JsdM0BG6Nkc2pNoW-qcG9wdWxhcml0ealzb3VyY2VfaWQBomRzzQEvo21yac0OLg==&listtype=pc&filters=[]&sorted_on=pricelth&slug=sports-energy-drinks&tab_type=[%22all%22]&page=1',
     vars: {
         'input_type' => 'taxonomy',
         'search_term' => '-',
@@ -14,11 +14,12 @@ pages << {
 
 search_terms = ['Red Bull', 'RedBull', 'Energidryck', 'Energidrycker']
 search_terms.each do |search_term|
+  break
 
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://www.bigbasket.com/product/get-products/?sid=EE0z4Y6hYwGjbWF2pTMuMi4wom5mw6hza3VfbGlzdJCiYW_ConVywqFxqHJlZCBidWxsomFww6JsdM0BG6Nkc2pNoW-pcmVsZXZhbmNlqXNvdXJjZV9pZAGiZHPNAS-jbXJpzQ4u&listtype=ps&filters=[]&sorted_on=popularity&slug=#{CGI.escape(search_term)}&tab_type=[%22all%22]",
+      url: "https://www.bigbasket.com/product/get-products/?sid=EE0z4Y6hYwGjbWF2pTMuMi4wom5mw6hza3VfbGlzdJCiYW_ConVywqFxqHJlZCBidWxsomFww6JsdM0BG6Nkc2pNoW-pcmVsZXZhbmNlqXNvdXJjZV9pZAGiZHPNAS-jbXJpzQ4u&listtype=ps&filters=[]&sorted_on=popularity&slug=#{CGI.escape(search_term)}&tab_type=[%22all%22]&page=1",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
