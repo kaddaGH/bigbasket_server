39823fbf07c91afc4a5734006fae8a2ed142531d
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-21 16:26:58 +0100

    update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -5,8 +5,9 @@ require 'cgi'
 
 pages << {
     page_type: 'products_search',
+    headers:headers,
     method: 'GET',
-    url: 'https://www.bigbasket.com/product/get-products/?sid=Sij0p4yhYwGjbWF2pTMuMi4wqHNrdV9saXN0kKJhb8KidXLCoXGocmVkIGJ1bGyiYXDDomx0zQEToW-pcmVsZXZhbmNlqXNvdXJjZV9pZAGibmbDo21yaQE==&listtype=pc&filters=[]&sorted_on=popularity&slug=sports-energy-drinks&tab_type=[%22all%22]',
+    url: 'https://www.bigbasket.com/product/get-products/?listtype=pc&filters=[]&sorted_on=popularity&slug=sports-energy-drinks&tab_type=[%22all%22]',
     vars: {
         'input_type' => 'taxonomy',
         'search_term' => '-',
@@ -20,7 +21,8 @@ search_terms.each do |search_term|
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://www.bigbasket.com/product/get-products/?sid=Sij0p4yhYwGjbWF2pTMuMi4wqHNrdV9saXN0kKJhb8KidXLCoXGocmVkIGJ1bGyiYXDDomx0zQEToW-pcmVsZXZhbmNlqXNvdXJjZV9pZAGibmbDo21yaQE=&listtype=ps&filters=[]&sorted_on=popularity&slug=#{CGI.escape(search_term)}&tab_type=[%22all%22]",
+      headers:headers,
+      url: "https://www.bigbasket.com/product/get-products/?listtype=ps&filters=[]&sorted_on=popularity&slug=#{CGI.escape(search_term)}&tab_type=[%22all%22]",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
