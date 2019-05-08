748d2c7d13578634404e12c738ea05e38ea75d20
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-05-08 13:33:08 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -31,7 +31,6 @@ pages << {
 }
 search_terms = ['Red Bull', 'Energy Drink', 'Energy Drinks']
 search_terms.each do |search_term|
-  break
   pages << {
       page_type: 'products_search',
       method: 'GET',
