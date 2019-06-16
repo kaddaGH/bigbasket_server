2c805bbdb8cae860a6b96b7daf9b860a3029323c
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-06-16 21:42:03 +0100

    Update header.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -7,7 +7,7 @@ headers = {
     'Accept-Encoding' => 'gzip, deflate, br',
     'Accept-Language' => 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5',
     'Connection' => 'keep-alive',
-    'Cookie' => '_bb_cid=1; _bb_vid="MjM4MzE3NTU2MA=="; _bb_tc=0; _bb_rdt="MzE1MjgwNDUyOQ==.0"; _bb_rd=6; sessionid=hd7i1lbaqntxcdyjsb225w8spsmpmyu9; _ga=GA1.2.1253197919.1557284407; _gcl_au=1.1.1861207601.1557284412; _fbp=fb.1.1557284416495.450514309; _bb_source=pwa; _client_version=2161; _vz=viz_5cd2b8211e2e1; _gid=GA1.2.230885793.1557677793; _bb_visaddr="fFZhc2FudGggTmFnYXJ8NzcuNTkyMDI5NXwxMi45OTEwODY5fDU2MDA1Mnw="; _bb_aid="MzAxNzU1NjE5Nw=="; csrftoken=a4bIg4LAC7b6yfBuVxwSOWpHdq5k2qZjEkUiUZy0RJ4RjZOo5RvIHWpAaREImPJL; _gat=1; bigbasket.com=3f6f2ea5-edbb-41b7-9780-cb89bc1f3b45; ts="2019-05-12 22:09:08.105"',
+    'Cookie' => '_bb_locSrc=default; _bb_cid=1; _bb_aid="MzE2OTE4OTIzMg=="; _bb_ftvid="MjM1OTk2NDM2Ng==|Yw0MTkMBBk4vEF5VUFJSCRBRV18DQVhWWhB3LjE="; _bb_vid="MjM1OTk2NDM2Ng=="; _bb_tc=0; _client_version=2171; _bb_rdt="MzE1Mjg4NjA3OA==.0"; _bb_rd=6; sessionid=4yv2ko4jrq829uhh0znrf8f2gxqrhko5; _ga=GA1.2.376097631.1560717593; _gid=GA1.2.1048238646.1560717593; _gat=1; _gcl_au=1.1.1545706021.1560717594; _fbp=fb.1.1560717598734.1937634291; bigbasket.com=447c4a0f-6681-4173-9ffa-26618755eec6; ts="2019-06-17 02:10:23.652"',
     'Host' => 'www.bigbasket.com',
     'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
     'X-CSRFToken' => 'a4bIg4LAC7b6yfBuVxwSOWpHdq5k2qZjEkUiUZy0RJ4RjZOo5RvIHWpAaREImPJL',
